c354c590716b80e2a42913493b090c0a6f5b6fcf
Craig Baker <craig.d.baker@gmail.com>
2011-11-08 09:57:40 -0600

    first commit

---
+++ app/cache/dev/assetic/config/8/8b21d8b72b9dce0a80fc01beaa7976c4.php
@@ -0,0 +1,5 @@
+<?php
+
+// MereFaithRBWordsBundle:Default:index.html.twig
+return array (
+);
---
+++ src/MereFaith/RBWordsBundle/MereFaithRBWordsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MereFaith\RBWordsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MereFaithRBWordsBundle extends Bundle
+{
+}
